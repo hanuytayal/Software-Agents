b995732e17c1151096673b318382cc5a5e431418
HalukProductions <101473183+HalukProductions@users.noreply.github.com>
2024-06-15 10:26:02 -0700

    Updated the skeleton code to include the following files:

--- agents.py
+++ agents.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 #Agent Manager
 
 #Agent Tester
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 #Launch
+from dotenv import load_dotenv
+load_dotenv()
+
+from crewai import Crew
+
+from tasks import Tasks
+from agents import Agents
+
+
+tasks = Tasks()
+agents = Agents()
+
+#Create a Crew object
+
+
+#Create Agents
+
+
+#Assign tasks to agents
+
+
+#Run the agents with sequential execution
+
+#Start the agents
+
+#Print the results
